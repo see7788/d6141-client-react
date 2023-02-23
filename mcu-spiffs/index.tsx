2d1aee63efb57376094301d525cea866c143bfb6
DIY呀 <diyya@qq.com>
2023-02-23 11:10:44 +0800

    111111

--- mcu-spiffs/index.tsx
+++ mcu-spiffs/index.tsx
@@ -1,7 +1,7 @@
 import 'antd/dist/reset.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Serial';
+import App from './App';
 ReactDOM.render(
 	<React.StrictMode><App /></React.StrictMode>,
 	document.getElementById('root')
