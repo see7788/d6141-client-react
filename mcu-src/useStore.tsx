1fdd1306d46df130c80263cb3ac9132aa12abcc8
DIY呀 <diyya@qq.com>
2023-03-07 22:43:37 +0800

    111111111111

--- mcu-src/useStore.tsx
+++ mcu-src/useStore.tsx
@@ -1,28 +1,103 @@
-import reactstore from "../lib/react-usestore"
+import { immer } from 'zustand/middleware/immer'
+import { create } from "zustand"
+import _ from "lodash"
 import globalConfig from "../globalConfig.json";
 type PowerPartial<T> = { [U in keyof T]?: T[U] extends object ? PowerPartial<T[U]> : T[U]; }
-export type State = (typeof globalConfig) & {
+type State = (typeof globalConfig) & {
     ybl: {
-        db: {
-            [i: string]: [//id
-                number, //定义排序
-                boolean, //state开关状态
-                number//yblRes.doTypes[number]用途
-            ]
-        }
+        db: Record<string, [//id
+            number, //const排序
+            boolean, //state开关状态
+            number//yblRes.doTypes[number]用途
+        ]>
     }
 }
-
-type ReqParam = {
-    state_merge?: PowerPartial<State>;
-    state_replace?: Partial<State>;
-    api_globalConfig_set?: Partial<State>,
-    api_globalConfig_get?: true,
-    api_globalConfig_toFile?: true,
-    api_globalConfig_fromFile?: true,
-    api_espRestart?: true
-};
-export default reactstore<ReqParam, State>(globalConfig as State)
+type Store = {
+    ipc:{
+        webSocketInit:(wsuri: `${"ws://" | "wss://"}${string}`)=>Promise<void>
+        stateInitSuccess?:true
+        workIng?:"webSocket"
+        //ipcSet:(ipcNow:"webSocket")=>Promise<void>
+    };
+    res: <api extends (keyof State) >(op: { api: api, db: State[api] } | { api: "globalConfig", db: State }) => void
+    req: <T extends keyof State>(
+        ...op: ["api_config_get", keyof State] |
+        ["api_config_set", T, State[keyof State]] |
+        ["api_globalConfig_set", Partial<State>] |
+        ["api_globalConfig_get"] |
+        ["api_globalConfig_toFile"] |
+        ["api_globalConfig_fromFile"] |
+        ["api_espRestart"]
+    ) => void;
+    state: State
+}
+export default create<Store>()(immer<Store>((set, self) => {
+    let ws: WebSocket;
+    let req: Store["req"]
+    const res: Store["res"] = ({ api, db }) => set(s => {
+        if (api === "globalConfig") {
+            s.ipc.stateInitSuccess=true;
+            s.state = { ...s.state, ...db }
+        } else if (api in s.state) {
+            s.state = { ...s.state, ...db }
+        } else {
+            console.log(api + ";web pass");
+        }
+    })
+    const loingStateSuccess = ():Promise<void> => new Promise((ok) => {
+        const loop = setInterval(() => {
+            if (self().ipc.stateInitSuccess) {
+                clearInterval(loop)
+                ok();
+            }
+        }, 500);
+    })
+    const wsInit:Store["ipc"]["webSocketInit"]=(wsuri)=> new Promise((ok) => {
+        ws = new WebSocket(wsuri);
+        ws.onopen = e => {
+            const loop = setInterval(() => {
+                if (ws.readyState === 1) {
+                    req = (...op) => {
+                        const [api, ...db] = op;
+                        ws.send(JSON.stringify({ api, db }))
+                    }
+                    set(s=>{
+                       s.ipc.workIng="webSocket";
+                    })
+                    clearInterval(loop)
+                    console.log("ws.onopen", "成功连接")
+                    ok();
+                }
+            }, 1000);
+        }
+        ws.onmessage = e => {
+            try {
+                const obj = JSON.parse(e.data);
+                res(obj)
+            } catch (e) {
+                console.error("ws.onmessage", e)
+            }
+        };
+        ws.onclose = e => {
+            console.error("ws.onclose", e);
+            set(s=>{
+               delete s.ipc.workIng;
+            })
+            setTimeout(() => {
+                wsInit(wsuri);
+            }, 2000);
+        }
+    })
+    return {
+        ipc:{
+            webSocketInit:(c)=>wsInit(c).then(()=>req("api_globalConfig_get")).then(loingStateSuccess),
+           // ipcSet:async()=>{}
+        },
+        res,
+        req: (...op) => req(...op),
+        state: globalConfig as State
+    }
+}))
 // let person= ['张三', '李四', '王五'];
 // console.log(person.unshift('小明')); // 头部插入，返回数组长度
 //person.pop();//删除最后一个
--- mcu-src/ybl/db.tsx
+++ mcu-src/ybl/db.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import store from "../useStore";
 import _ from "lodash"
 import {
@@ -22,28 +22,25 @@ import {
     UserAddOutlined,
     FormOutlined
 } from '@ant-design/icons';
-const UiyblState: FC = () => {
-    const dbs = store(s => s.state.ybl.db);
+const App: FC = () => {
+    const ybl = store(s => s.state.ybl);
+    const dbs = ybl.db;
     const db_entries = Object.entries(dbs)
     const doTypes = store(s => s.state.ybl.doTypes);
     const del = (id: string) => store.setState(s => {
         delete s.state.ybl.db[id];
-        s.req({ api_globalConfig_set: { ybl: s.state.ybl } })
-    });
-    const setOrderBy = (id: string, by: number) => store.setState(s => {
-        s.state.ybl.db[id][0] = by
-        s.req({ api_globalConfig_set: { ybl: s.state.ybl } })
+        // console.log(id,s.state.ybl)
+        // s.req({ api_globalConfig_set: { ybl: s.state.ybl } })
     });
     const setDoType = (id: string, doId: number) => store.setState(s => {
-        s.state.ybl.db[id][2] = doId
-        s.req({ api_globalConfig_set: { ybl: s.state.ybl } })
+        s.state.ybl.db[id][2]=doId;
+        //s.req(["api_config_set",ybl])
     });
-    //先按用途，再按定义排序
-    // c => `${c[1][2]}${c[1][0]}`
-    const children = _.orderBy(db_entries).map(([id, info]) => {
+    const children = _.orderBy(db_entries, e => e[1][0]).map(([id, info]) => {
         const [byNumber, state, doId] = info as [number, boolean, number];
         const doname = doTypes[doId]
         const style = { backgroundColor: state ? '#FF3366' : "#33CC33" }
+        const doIdSet=(value:string)=>setDoType(id, Number(value))
         return (
             <Badge
                 key={id}
@@ -53,22 +50,10 @@ const UiyblState: FC = () => {
                 <Popover
                     content={
                         <Space wrap={true}>
-                            <Button onClick={() => del(id)}>删除<DeleteOutlined /></Button >
+                            <Button onClick={() => del(id)}>删传感器<DeleteOutlined /></Button >
                             <Select
-                                onChange={(value) => setOrderBy(id, Number(value))}
-                                defaultValue="修改编号"
-                                options={
-                                    [...Array(db_entries.length)].map((v, i) => {
-                                        ++i
-                                        return {
-                                            value: i,
-                                            label: byNumber === i ? `*${i}*` : i,
-                                        }
-                                    })
-                                } />
-                            <Select
-                                onChange={(value) => setDoType(id, Number(value))}
-                                defaultValue="修改用途"
+                                onChange={doIdSet}
+                                defaultValue="设定用途"
                                 options={doTypes.map((v, i) => ({
                                     value: i,
                                     label: doId === i ? `*${v}*` : v,
@@ -88,4 +73,4 @@ const UiyblState: FC = () => {
     })
     return <Space key={1} wrap={true} align="end" children={children} />
 }
-export default UiyblState
+export default App
