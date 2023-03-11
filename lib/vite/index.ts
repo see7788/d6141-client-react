8fef6e1276df4b126ba615d6f3e57b419c289850
DIY呀 <diyya@qq.com>
2023-03-11 20:04:20 +0800

    11111111

--- lib/vite/index.ts
+++ lib/vite/index.ts
@@ -1,15 +1,12 @@
-import { normalizePath, defineConfig } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import basicSsl from '@vitejs/plugin-basic-ssl'//vite-plugin-mkcert //ssl证书
 import viteCompression from 'vite-plugin-compression';
-// import copyFiles from 'rollup-plugin-copy'//没有拷贝
 import path from "path"
 import fs from "fs"
 const cwdPath = process.cwd()
-normalizePath(cwdPath);
 const apps = fs.readdirSync(cwdPath).
   filter(name => fs.lstatSync(name).isDirectory()).
-  filter(c => !(c.indexOf("-build") > -1 || ["mcu-src", "node_modules"].indexOf(c) > -1))
+  filter(c =>!(c.indexOf("-build")>-1|| ["mcu-src","node_modules"].indexOf(c) > -1))
 export default defineConfig((param) => {
   const isBuild = param.command === "build"
   const site = param.mode;
@@ -17,20 +14,14 @@ export default defineConfig((param) => {
     throw new Error(`must --mode=${apps.join("|")}`)
   }
   const sitePath = path.resolve(cwdPath, site)
-  const buildDirName=`${site}-build`
-  const buildToPath = path.resolve(cwdPath, buildDirName)
+  const buildToPath = path.resolve(cwdPath, `${site}-build`)
   const indexHtmlPath = path.resolve(sitePath, "index.html")
   console.log({ isBuild, site, cwdPath, sitePath, buildToPath, indexHtmlPath })
   return {
     root: sitePath,
     plugins: [
       react(),
-      // copyFiles({
-      //   targets: [
-      //     { src: '/globalConfig.json', dest: buildDirName }
-      //   ]
-      // }),
-      //viteCompression({ deleteOriginFile: true })
+      viteCompression({deleteOriginFile:true})
       // visualizer({ open: true })
     ],
     server: {
@@ -45,7 +36,7 @@ export default defineConfig((param) => {
           drop_debugger: isBuild,//清debugger
         },
       },
-
+    
       emptyOutDir: true,//打包前清空
       outDir: buildToPath,
       rollupOptions: {
